894bdffc9441285105725aa57db4bcccf770ccf9
cencibuqi <2773615936@qq.com>
2024-09-29 16:25:58 +0800

    first version

---
+++ src/components/MyList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="todo-main">
+    <MyItem v-for="item in todos" :key="item.id" :todo="item" :checkTodo="checkTodo" :delTodo="delTodo" />
+  </ul>
+</template>
+
+<script>
+import MyItem from './MyItem'
+
+export default {
+  name: 'MyList',
+  components: {
+    MyItem
+  },
+  props: ['todos', 'checkTodo', 'delTodo']
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
